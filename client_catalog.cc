e41a970fe8af1dd2c8ff4be726d0f22f40a8c80f
Seoyeong <iseoyeong@Seoyeongs-MacBook-Air.local>
2024-06-06 12:46:04 +0900

    catalog #4 (add comments)

--- client_catalog.cc
+++ client_catalog.cc
@@ -6,15 +6,13 @@
 #include <stdio.h>
 #include <string>
 #include <chrono>
-
-
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <fcntl.h>
 #include <mutex>
-
 #include <lustre/lustreapi.h>
+
 #define MAX_FILE_LEN 512
 #define NUM_ELEMENTS 500000
 #define SHARED_MEMORY_KEY 1234000
@@ -27,15 +25,14 @@
 
 using namespace std;
 
-char current_working_directory[PATH_MAX];
-
-typedef struct Element {
+typedef struct Element{
     char msg[MSG_SIZE];
     bool filled;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
-}Element;
-struct Element* msg_queue;
+} Element;
+
+struct Element *msg_queue;
 
 int shmid;
 static int Rank, worldSize;
@@ -46,88 +43,96 @@ bool initial_call = true;
 
 char *env_home;
 int process_id;
-
+char current_working_directory[PATH_MAX];
 
 void init_environ_var();
 void init_msg_queue();
 
 int (*origin_close)(int fd);
-int (*origin_open)(const char * file_path, int flags, ...);
+int (*origin_open)(const char *file_path, int flags, ...);
 
-void get_full_path(const char *path, char *full_path) {
-    if (path[0] == '/') {
+// get the full path in case of relative path
+void get_full_path(const char *path, char *full_path){ 
+    if (path[0] == '/'){
         strncpy(full_path, path, PATH_MAX);
-    } else {
+    }
+    else{
         strncpy(full_path, current_working_directory, PATH_MAX);
         strncat(full_path, "/", PATH_MAX - strlen(full_path) - 1);
         strncat(full_path, path, PATH_MAX - strlen(full_path) - 1);
     }
 }
 
+// custom open
+int open(const char *pathname, int flags, ...){
 
-int open(const char *pathname, int flags, ...) {
+    origin_open = (int (*)(const char *, int, ...))dlsym(RTLD_NEXT, "open");
 
-     origin_open = (int (*)(const char*, int, ...))dlsym(RTLD_NEXT, "open");
+    // shared memory and environment setting
     if (initial_call){
         initial_call = false;
         init_environ_var();
         init_msg_queue();
-        if (getcwd(current_working_directory, PATH_MAX) == NULL) {
+        if (getcwd(current_working_directory, PATH_MAX) == NULL){
             perror("getcwd");
             return -1;
         }
     }
+
     char full_path[PATH_MAX];
     get_full_path(pathname, full_path);
-    if (string(full_path).find(ROOT_PATH) == string::npos) {
-        if (flags & O_CREAT) {
+
+    // not intercepted form my program (don't know why)
+    if (string(full_path).find(ROOT_PATH) == string::npos){
+        if (flags & O_CREAT){
             va_list args;
             va_start(args, flags);
             mode_t mode = va_arg(args, mode_t);
             va_end(args);
             return origin_open(pathname, flags, mode);
-        } else {
+        }
+        else{
             return origin_open(pathname, flags);
         }
     }
     int fd;
 
-    if ((flags & O_CREAT)|| (flags & O_RDWR) || (flags & O_WRONLY)) {
- //       if ((flags & O_CREAT)) {
+    // from my application
+    if ((flags & O_CREAT) || (flags & O_RDWR) || (flags & O_WRONLY)){
         va_list args;
         va_start(args, flags);
         mode_t mode = va_arg(args, mode_t);
         va_end(args);
         fd = origin_open(full_path, flags, mode);
-        if(fd >=0){
+
+        // write message to the server
+        if (fd >= 0){
             Element *element = &msg_queue[head_idx];
             snprintf(element->msg, MSG_SIZE, "OPEN\t%s\t%d\t%d", full_path, fd, flags);
             element->filled = true;
             head_idx = (head_idx + NUMPERNODE) % NUM_ELEMENTS;
         }
-
     }
+    // proceed with the original open
     else{
         fd = origin_open(full_path, flags);
     }
     return fd;
 }
 
-int close(int fd)
-{
+int close(int fd){
     char file_path[MAX_FILE_LEN] = {0};
     string _file_path;
     int shmid;
 
     origin_close = (int (*)(int))dlsym(RTLD_NEXT, "close");
 
-    string descriptor_path = "/proc/self/fd/" + to_string(fd); 
-    readlink(descriptor_path.c_str(), file_path, MAX_FILE_LEN); 
-    _file_path = file_path; 
+    string descriptor_path = "/proc/self/fd/" + to_string(fd);
+    readlink(descriptor_path.c_str(), file_path, MAX_FILE_LEN);
+    _file_path = file_path;
 
     if (_file_path.find(ROOT_PATH) == string::npos)
         return (*origin_close)(fd);
-
 
     Element *element = &msg_queue[head_idx];
     snprintf(element->msg, MSG_SIZE, "CLOSE\t%d", fd);
@@ -137,22 +142,18 @@ int close(int fd)
     return (*origin_close)(fd);
 }
 
-void init_environ_var()
-{
-    char * env_rank = getenv("OMPI_COMM_WORLD_RANK");
-    char * env_size = getenv("OMPI_COMM_WORLD_SIZE");
+void init_environ_var(){
+    char *env_rank = getenv("OMPI_COMM_WORLD_RANK");
+    char *env_size = getenv("OMPI_COMM_WORLD_SIZE");
     process_id = getpid();
 
-    if (!env_rank || !env_size)
-    {
+    if (!env_rank || !env_size){
         puts("Getting environment variable failed.");
         exit(0);
     }
 
-
     char *hostname = getenv("HOSTNAME");
-    if (hostname != NULL)
-    {
+    if (hostname != NULL){
         puts("client");
         puts(hostname);
         puts(env_rank);
@@ -162,23 +163,21 @@ void init_environ_var()
     Rank = atoi(env_rank);
     worldSize = atoi(env_size);
     head_idx = Rank % NUMPERNODE;
-
-
-
 }
 
 void init_msg_queue(){
     int node_idx = Rank / MPIPROCS;
-    key_t key = ftok(".", SHARED_MEMORY_KEY+node_idx);
-    shmid = shmget(key, NUM_ELEMENTS * sizeof(struct Element),  0666);
-     if (shmid == -1) {
-            perror("shmget");
+
+    // processes from the same node share the array buffer
+    key_t key = ftok(".", SHARED_MEMORY_KEY + node_idx);
+    shmid = shmget(key, NUM_ELEMENTS * sizeof(struct Element), 0666);
+    if (shmid == -1){
+        perror("shmget");
         exit(1);
     }
 
-
     msg_queue = (struct Element *)shmat(shmid, NULL, 0);
-    if (msg_queue == (void *)-1) {
+    if (msg_queue == (void *)-1){
         perror("shmat");
         exit(1);
     }
@@ -189,11 +188,10 @@ void init_msg_queue(){
     puts("shared memory was successfully attached on");
     process_id = getpid();
     char pid_msg[MSG_SIZE];
+    // send the pid to the server for server program shutdown
     snprintf(pid_msg, MSG_SIZE, "PID\t%d", process_id);
     Element *element = &msg_queue[head_idx];
     strncpy(element->msg, pid_msg, MSG_SIZE);
     element->filled = true;
     head_idx = (head_idx + NUMPERNODE) % NUM_ELEMENTS;
-
 }
-
--- mpi_server_catalog.cc
+++ mpi_server_catalog.cc
@@ -25,33 +25,32 @@
 #define NUM_ELEMENTS 500000
 #define NUMPERNODE 3
 
-
 using namespace std;
 
 struct Element *msg_queue;
-FILE * catalog_fp;
+FILE *catalog_fp;
 int shmid;
 int tail_idx = 0;
 
-typedef struct Element {
+typedef struct Element{
     char msg[MSG_SIZE];
     bool filled;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
-}Element;
+} Element;
 
-struct OpenFileInfo {
+struct OpenFileInfo{
     string path;
     int flags;
     struct stat64 sb;
 };
-
 
 static int processed_cnt = 0;
 static int cnt = 0;
 int Rank, worldSize;
 bool real_dead = false;
 
+// for server shutdown 
 map<int, OpenFileInfo> open_files;
 vector<string> catalog_entries;
 vector<pid_t> client_pids;
@@ -63,7 +62,7 @@ void check_alive();
 void write_catalog();
 void record_fstat(const char *file_path, const struct stat64 *sb);
 
-int main() {
+int main(){
 
     printf("%lu\n", NUM_ELEMENTS * sizeof(struct Element));
     int provided;
@@ -74,153 +73,139 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
 
-    char * string_env_rank = getenv("OMPI_COMM_WORLD_RANK");
+    char *string_env_rank = getenv("OMPI_COMM_WORLD_RANK");
     int env_rank = atoi(string_env_rank);
-    printf("rank: %d env_rank: %d processID: %d\n", Rank, env_rank,  getpid());
+    printf("rank: %d env_rank: %d processID: %d\n", Rank, env_rank, getpid());
 
     int node_idx = Rank / NUMPERNODE;
     char hostname[256];
-        gethostname(hostname, sizeof(hostname));
+    gethostname(hostname, sizeof(hostname));
     printf("%d\t%s\t", Rank, hostname);
 
-
-    if (Rank % NUMPERNODE != 0)
-    {
+    if (Rank % NUMPERNODE != 0){
         MPI_Finalize();
         return 0;
     }
 
     msg_queue_init();
 
+    // for shutdown
     thread check_thread(check_alive);
 
     string catalog_file = "catalog" + to_string(node_idx) + ".txt";
     puts(catalog_file.c_str());
 
     catalog_fp = fopen(catalog_file.c_str(), "w");
-    if (catalog_fp == NULL)
-    {
+    if (catalog_fp == NULL){
         perror("Open catalog");
         exit(1);
     }
 
-
-    char file_path [MSG_SIZE];
+    char file_path[MSG_SIZE];
 
     string _file_path;
 
-
-    while(1){
-
+    while (1){
         if (msg_queue[tail_idx].filled == true){
 
             strncpy(file_path, msg_queue[tail_idx].msg, MSG_SIZE);
             printf("receieved %s\n", file_path);
-            if (strncmp(file_path, "PID\t", 4) == 0) {
+            if (strncmp(file_path, "PID\t", 4) == 0){
                 pid_t pid;
                 sscanf(file_path, "PID\t%d", &pid);
                 client_pids.push_back(pid);
             }
-            else if (strncmp(file_path, "OPEN\t", 5) == 0) {
+            else if (strncmp(file_path, "OPEN\t", 5) == 0){
                 char path[PATH_MAX];
                 int fd, flags;
                 sscanf(file_path, "OPEN\t%s\t%d\t%d", path, &fd, &flags);
                 struct stat64 sb;
+                // call fstat once OPEN (because only newly created or modified open msg were sent)
                 fstat64(fd, &sb);
                 open_files[fd] = {string(path), flags, sb};
             }
 
             else if (strncmp(file_path, "CLOSE\t", 6) == 0){
-                  int fd;
+                int fd;
                 sscanf(file_path, "CLOSE\t%d", &fd);
                 if (open_files.find(fd) != open_files.end()){
                     OpenFileInfo info = open_files[fd];
                     open_files.erase(fd);
                     record_fstat(info.path.c_str(), &info.sb);
-                   if(info.flags & O_CREAT){
-                    if (layout_analysis(info.path.c_str(), &info.sb) == 0) {
-     
-                        catalog_entries.push_back(info.path);
-                        if (catalog_entries.size() >= 20) {
-                            write_catalog();
+                    // layout check only for the newly created file (as layout does not change)
+                    if (info.flags & O_CREAT){
+                        if (layout_analysis(info.path.c_str(), &info.sb) == 0){
+                            catalog_entries.push_back(info.path);
+                            // periodically flush
+                            if (catalog_entries.size() >= 20){
+                                write_catalog();
+                            }
                         }
                     }
-                    }
-
                 }
             }
             msg_queue[tail_idx].filled = false;
             processed_cnt++;
             tail_idx = (tail_idx + 1) % NUM_ELEMENTS;
-
-
         }
-        if(real_dead == true){
+        if (real_dead == true){
             break;
         }
-
-
-
     }
 
-       MPI_Finalize();
-       
-            fclose(catalog_fp);
-                if (shmdt(msg_queue) == -1) {
-                    perror("shmdt");
-                    exit(1);
-                }
+    MPI_Finalize();
 
-                if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-                    perror("shmctl");
-                    exit(1);
-                }
+    fclose(catalog_fp);
+    if (shmdt(msg_queue) == -1){
+        perror("shmdt");
+        exit(1);
+    }
 
-                printf("processed cnt: %d\n", processed_cnt);
-                puts("Server Process Completed");
-                exit(0);
+    if (shmctl(shmid, IPC_RMID, NULL) == -1){
+        perror("shmctl");
+        exit(1);
+    }
+
+    printf("processed cnt: %d\n", processed_cnt);
+    puts("Server Process Completed");
+    exit(0);
 
     return 0;
 }
 
-
-void msg_queue_init()
-{
+void msg_queue_init(){
     int node_idx = Rank / NUMPERNODE;
-    key_t key = ftok(".", SHARED_MEMORY_KEY+node_idx);
-    
+    key_t key = ftok(".", SHARED_MEMORY_KEY + node_idx);
+
     shmid = shmget(key, NUM_ELEMENTS * sizeof(struct Element), IPC_CREAT | 0666);
-    if (shmid == -1) {
+    if (shmid == -1){
         perror("shmget");
         exit(1);
     }
     printf("id: %d\n", shmid);
 
     msg_queue = (struct Element *)shmat(shmid, NULL, 0);
-    if (msg_queue == (void *)-1) {
+    if (msg_queue == (void *)-1){
         perror("shmat");
         exit(1);
     }
 
-    for (int i = 0; i < NUM_ELEMENTS; ++i) {
+    for (int i = 0; i < NUM_ELEMENTS; ++i){
         msg_queue[i].filled = false;
-    pthread_mutex_init(&msg_queue[i].mutex, NULL);
-    pthread_cond_init(&msg_queue[i].cond, NULL);
-     }
+        pthread_mutex_init(&msg_queue[i].mutex, NULL);
+        pthread_cond_init(&msg_queue[i].cond, NULL);
+    }
 
     printf("shared memory has created well\n");
 
-    char * hostname = getenv("HOSTNAME");
+    char *hostname = getenv("HOSTNAME");
     printf("server: %s\n", hostname);
-
 }
 
-int catalog_append(const char *file_path, int idx, uint64_t start, uint64_t end, uint64_t interval, uint64_t size) {
+int catalog_append(const char *file_path, int idx, uint64_t start, uint64_t end, uint64_t interval, uint64_t size){
     static int cnt = 0;
     char buf[512];
-    snprintf(buf, sizeof(buf), "%d %s %d %" PRIu64 " %" PRIu64 " %" PRIu64 " %" PRIu64, cnt, file_path, idx, start, end, interval, size);
-    {
- 
+    snprintf(buf, sizeof(buf), "%d %s %d %" PRIu64 " %" PRIu64 " %" PRIu64 " %" PRIu64, cnt, file_path, idx, start, end, interval, size);{
         catalog_entries.push_back(string(buf));
     }
     printf("%s\n", buf);
@@ -228,47 +213,41 @@ int catalog_append(const char *file_path, int idx, uint64_t start, uint64_t end,
     return 0;
 }
 
-
-
 int layout_analysis(const char *file_path, struct stat64 *sb){
 
-    struct llapi_layout * layout;
+    struct llapi_layout *layout;
 
     int rc[3];
-    uint64_t count, ost,  start, end, size, file_size, interval ;
+    uint64_t count, ost, start, end, size, file_size, interval;
 
     layout = llapi_layout_get_by_path(file_path, 0);
     if (layout == NULL){
         puts(file_path);
-        perror("layout error");;
+        perror("layout error");
+        ;
         return 1;
     }
 
-
     file_size = sb->st_size;
-
 
     rc[0] = llapi_layout_comp_use(layout, 1);
     if (rc[0]){
         printf("error: layout component iteration failed\n");
-        return 1 ;
+        return 1;
     }
-    while(1){
+    while (1){
         rc[0] = llapi_layout_stripe_count_get(layout, &count);
         rc[1] = llapi_layout_stripe_size_get(layout, &size);
         rc[2] = llapi_layout_comp_extent_get(layout, &start, &end);
-        if (rc[0] || rc[1] || rc[2])
-        {
+        if (rc[0] || rc[1] || rc[2]){
             printf("error: cannot get stripe information\n");
             continue;
         }
 
         interval = count * size;
-        end = (end < file_size)? end : file_size;
-        for (int i = 0; i < count; i ++)
-        {
+        end = (end < file_size) ? end : file_size;
+        for (int i = 0; i < count; i++){
 
-            
             rc[0] = llapi_layout_ost_index_get(layout, i, &ost);
             if (rc[0]){
                 goto here_exit;
@@ -284,70 +263,67 @@ int layout_analysis(const char *file_path, struct stat64 *sb){
             printf("error: layout component iteration failed\n");
         break;
     }
-    here_exit:
-        return  0;
-
+here_exit:
+    return 0;
 }
 
-void sig_kill_handler(int signal)
-{
+void sig_kill_handler(int signal){
     fclose(catalog_fp);
 
-    if (shmdt(msg_queue) == -1) {
+    if (shmdt(msg_queue) == -1){
         perror("shmdt");
         exit(1);
     }
 
-    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+    if (shmctl(shmid, IPC_RMID, NULL) == -1){
         perror("shmctl");
         exit(1);
     }
-
 
     printf("processed cnt: %d\n", processed_cnt);
     puts("Server Process Completed");
     exit(0);
 }
-void check_alive() {
+void check_alive(){
 
-    while(true){
-        if(processed_cnt > 0){
+    while (true){
+        if (processed_cnt > 0){}
             break;
         }
     }
+    // once the file creation/update starts, start monitoring
     printf("escape\n");
-    while (true) {
+    while (true){
         this_thread::sleep_for(chrono::seconds(5));
 
-
-        if (open_files.empty()) {
+        // check if there is no file currently opened
+        if (open_files.empty()){
             bool all_dead = true;
-            for (pid_t pid : client_pids) {
-                if (kill(pid, 0) != -1 || errno != ESRCH) {
+            // check the pids of the client application
+            for (pid_t pid : client_pids){
+                if (kill(pid, 0) != -1 || errno != ESRCH){
                     all_dead = false;
                     break;
                 }
             }
-            if (all_dead) {
+            if (all_dead){
                 write_catalog();
                 real_dead = true;
             }
         }
     }
-
 }
 
-void write_catalog() {
-    for (const string &entry : catalog_entries) {
+void write_catalog(){
+    for (const string &entry : catalog_entries){
         fprintf(catalog_fp, "%s\n", entry.c_str());
     }
     fflush(catalog_fp);
     catalog_entries.clear();
 }
 
-void record_fstat(const char *file_path, const struct stat64 *sb) {
+void record_fstat(const char *file_path, const struct stat64 *sb){
     char buf[512];
     snprintf(buf, sizeof(buf), "fstat: %s %ld %ld %ld %ld %ld %ld %ld %ld %ld", file_path, (long)sb->st_dev, (long)sb->st_ino, (long)sb->st_mode, (long)sb->st_nlink, (long)sb->st_uid, (long)sb->st_gid, (long)sb->st_rdev, (long)sb->st_size, (long)sb->st_blksize, (long)sb->st_blocks);
     catalog_entries.push_back(string(buf));
 }
-
